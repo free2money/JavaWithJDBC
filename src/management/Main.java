785369db9ee19d1d11ca434cd82a508df406fc08
free2money <38486282+free2money@users.noreply.github.com>
2019-11-05 10:06:58 +0900

    test

--- src/management/Main.java
+++ src/management/Main.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		System.out.println("teste");
 		mainMenu();
 	}
 
